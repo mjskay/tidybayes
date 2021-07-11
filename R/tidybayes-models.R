5148c37451e4a37199df901d6487122eeeba87d8
Matthew Kay <matthew.kay@gmail.com>
2021-07-10 20:42:17 -0500

    update model type support docs

--- R/tidybayes-models.R
+++ R/tidybayes-models.R
@@ -16,7 +16,8 @@
 #' @section All Supported Models/Sample Formats:
 #'
 #' **All supported models/formats** support the base tidybayes sample extraction functions, such as
-#' [tidy_draws()], [spread_draws()], and [gather_draws()]. These models/formats include:
+#' [tidy_draws()], [spread_draws()], [gather_draws()], [spread_rvars()], and [gather_rvars()].
+#' These models/formats include:
 #'
 #' \itemize{
 #'   \item [rstan][rstan::stan] models
@@ -43,15 +44,21 @@
 #' @section Models Supporting Prediction:
 #'
 #' In addition, the **following models support fit and prediction** extraction functions, such as
-#' [add_fitted_draws()] and [add_predicted_draws()]:
+#' [add_epred_draws()], [add_predicted_draws()], [add_linpred_draws()], [add_epred_rvars()],
+#' [add_predicted_rvars()], and [add_linpred_rvars()]:
 #'
 #' \itemize{
 #'   \item [brms::brm()] models
 #'   \item [rstanarm][rstanarm::rstanarm-package] models
+#'   \item any package with implementations of [rstantools::posterior_epred],
+#'     [rstantools::posterior_predict], or [rstantools::posterior_linpred] that
+#'     include an argument called `newdata` which takes a data frame of predictors.
 #' }
 #'
 #' **If your model type is not in the above list**, you may still be able to use the [add_draws()]
-#' function to turn matrices of predictive draws (or fit draws) into tidy data frames.
+#' function to turn matrices of predictive draws (or fit draws) into tidy data frames. Or,
+#' you can wrap output from a prediction function in `posterior::rvar()` and add it
+#' to a data frame so long as that output is a matrix with draws as rows.
 #'
 #' If you install the [tidybayes.rethinking](https://mjskay.github.io/tidybayes.rethinking/) package, models from
 #' the [rethinking](https://github.com/rmcelreath/rethinking) package are also supported.
