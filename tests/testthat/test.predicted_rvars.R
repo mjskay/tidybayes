9f1c7083e1f54f3e59284e97d7a05077e90e5e82
Matthew Kay <matthew.kay@gmail.com>
2023-02-02 23:18:37 -0600

    minor test fixes

--- tests/testthat/test.predicted_rvars.R
+++ tests/testthat/test.predicted_rvars.R
@@ -84,10 +84,9 @@ test_that("[add_]predicted_rvars works on brms models with dirichlet responses",
 
   set.seed(1234)
   grid = tibble(x = c("A", "B"))
-  pred_draws = rstantools::posterior_predict(m_dirich, grid, ndraws = 10)
-  attr(pred_draws, "levels") = NULL
+  draws = rstantools::posterior_predict(m_dirich, grid, ndraws = 10)
   ref = grid %>%
-    mutate(.prediction = rvar(pred_draws))
+    mutate(.prediction = rvar(draws))
 
   expect_equal(predicted_rvars(m_dirich, grid, seed = 1234, ndraws = 10), ref)
 })
@@ -110,7 +109,7 @@ test_that("[add_]predicted_rvars works on brms models with multinomial responses
   column_ref = ref %>%
     mutate(.row = 1:n()) %>%
     group_by(across(-.prediction)) %>%
-    enframe(col_pred = colnames(.prediction), .prediction = t(.prediction)) %>%
+    reframe(col_pred = colnames(.prediction), .prediction = t(.prediction)) %>%
     arrange(col_pred, .row)
   dim(column_ref$.prediction) = length(column_ref$.prediction)
   attr(draws_of(column_ref$.prediction), "levels") = c("a","b","c")
